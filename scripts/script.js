93fea204fcf75d787e1440bd843d726f12f1149a
Lena Ryan <alissadyiphil@gmail.com>
2021-03-16 16:30:46 +0300

    add validation to the course form

--- scripts/script.js
+++ scripts/script.js
@@ -51,3 +51,25 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 $(() => {new WOW().init();})
+
+var btnSend = document.getElementById('course-submit');
+var formEmail = document.getElementById('course-form');
+var inputEmail = document.getElementById('course-email');
+btnSend.addEventListener("click", sendMessage);
+
+function sendMessage(e) {
+    e.preventDefault();
+    console.log(validateEmail(inputEmail));
+    if(validateEmail(inputEmail.value)) {
+        formEmail.classList.add('course-form--output');
+        formEmail.classList.remove('course-form--error');
+        inputEmail.value = '';
+    } else {
+        formEmail.classList.add('course-form--error');
+    }
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
